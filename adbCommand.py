0294a029bdbf2a5c2b55b1d922afd1a88b1a3a59
cvyw <cvyw.hongye@gmail.com>
2021-10-22 15:26:08 +0800

    完善

--- adbCommand.py
+++ adbCommand.py
@@ -6,15 +6,15 @@ devices = "adb devices"
 brevent = "adb -d shell sh /data/data/me.piebridge.brevent/brevent.sh"
 shizuku = "adb shell sh /storage/emulated/0/Android/data/moe.shizuku.privileged.api/start.sh"
 
-devices_num = os.popen(devices).read()
-if "successful" in devices_num:
-	print("连接成功")
-else:
+devicesIds = os.popen(devices).read().splitlines(False)
+if devicesIds[1] == '':
     print("连接失败")
     os.system("pause")
     os._exit(0)
+else:
+	print("连接成功")
     
-print(devices_num)
+print("已连接" + devicesIds[1])
 print("开启shizuku服务")
 os.popen(shizuku).read()
 print("开启黑阈服务")
